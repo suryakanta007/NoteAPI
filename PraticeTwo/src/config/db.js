2fa34ae20613751426b7e8a1a84b55d95f3fd1c3
suryakanta007 <sahusuryakanta073@gmail.com>
2026-05-30 10:46:56 +0530

    Add DB connection

---
+++ PraticeTwo/src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDB = async()=>{
+    try {
+        await mongoose.connect(process.env.DB_URL)
+        console.log("DB connected");
+        
+    } catch (error) {
+        throw new Error("DB connection error :"+error)
+    }
+}
+
+export default connectDB
--- PraticeTwo/src/server.js
+++ PraticeTwo/src/server.js
@@ -2,12 +2,20 @@ import http from "http";
 import app from "./app.js";
 import { config } from "dotenv";
 
+import  dbConnect  from "./config/db.js";
+
+
 
 config();
 
 const server = http.createServer(app);
 const PORT = process.env.PORT;
 
-server.listen(PORT, () => {
-    console.log(`server is runing on http://localhost:${PORT}`);
+dbConnect().then(() => {
+    server.listen(PORT, () => {
+        console.log(`server is runing on http://localhost:${PORT}`);
+    });
+}).catch((err) => {
+    console.log(err);
 });
+
